3b08d657c63e1c47e14e90fd3f9563425ab954c1
sparatanveto <pratapudaysingh@gmail.com>
2019-04-15 19:26:17 +0530

    Create Assignment12.java

---
+++ Assignment12.java
@@ -0,0 +1,48 @@
+
+//**************************************************************************
+//GOT THE SOLUTION
+//FOR SOLUTION CONTACT ME ON SPARTANVETO@GMAIL.COM
+//**************************************************************************
+
+
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class Assignment12 extends Application
+{
+   private final int WIDTH = 550;
+   private final int HEIGHT = 400;
+
+   public void start(Stage primaryStage)
+   {
+      //create a ControlPane object. See ControlPane.java for details.
+      ControlPane pane = new ControlPane(WIDTH, HEIGHT);
+
+      //put pane onto the rootPane
+      StackPane rootPane = new StackPane();
+      rootPane.getChildren().add(pane);
+
+      // Create a scene and place rootPane in the stage
+      Scene scene = new Scene(rootPane, WIDTH, HEIGHT);
+
+      primaryStage.setTitle("Fireworks");
+      primaryStage.setScene(scene); // Place the scene in the stage
+      primaryStage.show(); // Display the stage
+   }
+   public static void main(String[] args)
+   {
+      Application.launch(args);
+   }
+}
+
+
+
+//**************************************************************************
+//GOT THE SOLUTION
+//FOR SOLUTION CONTACT ME ON SPARTANVETO@GMAIL.COM
+//**************************************************************************
+
